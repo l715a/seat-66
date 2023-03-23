e72922e4a0036a0d2065cdf3cb7f4db3fbf96844
Andreas <Andreas.Haller@netcentric.biz>
2023-03-23 10:16:01 -0700

    add blockquote

---
+++ blocks/blockquote/blockquote.js
@@ -0,0 +1,13 @@
+export default async function decorate(block) {
+    const quoteDiv = block.querySelector(':scope > div > div');
+    const element = document.createElement('blockquote');
+    element.innerHTML = quoteDiv.innerHTML;
+    quoteDiv.replaceWith(element);
+
+    const authorDiv = block.querySelector(':scope > div:nth-of-type(2) > div');
+    if(authorDiv) {
+        const authorElement = document.createElement('p');
+        authorElement.innerHTML = `<strong><em>${authorDiv.innerHTML}</em></strong>`;
+        authorDiv.replaceWith(authorElement);
+    }
+}
